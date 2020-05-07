19db721c44017c3e2828759365ce2efe7f78396f
novijook <M.vanRoosmalen@noviuniversity.com>
2020-05-07 12:41:23 +0200

    commit useState project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(10);
+  const [{ count2, count3 }, setCount23] = useState({ count2: 15, count3: 30 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>count</h1>
+      <button onClick={() => setCount((c) => c + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <div>count: {count}</div>
+      <hr />
+      <h1>count 2,3</h1>
+      <div>count2: {count2}</div>
+      <div>count3: {count3}</div>
+      <button
+        onClick={() =>
+          setCount23((currentState) => ({
+            ...currentState,
+            count2: currentState.count2 + 1,
+          }))
+        }
+      >
+        increment count2
+      </button>
+      <button
+        onClick={() =>
+          setCount23((currentState) => ({
+            ...currentState,
+            count3: currentState.count3 + 1,
+          }))
+        }
+      >
+        increment count3
+      </button>
     </div>
   );
-}
+};
 
 export default App;
